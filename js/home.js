1a5cd82616b978f826a0829514b60753c15e8986
abhirimal <abhirimal009@gmail.com>
2021-05-29 16:09:45 +0545

    fix header and footer

--- js/home.js
+++ js/home.js
@@ -37,21 +37,4 @@ function next(direction){
 
 }
 
-//Sticky Nav
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
 
-// Get the navbar
-var nav = document.getElementById("myNav");
-
-// Get the offset position of the navbar
-var sticky = nav.offsetTop;
-
-// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    nav.classList.add("sticky")
-  } else {
-    nav.classList.remove("sticky");
-  }
-}
